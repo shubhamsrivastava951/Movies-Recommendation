8b2de2bac82969db8eab3461d239f13246589415
Shubham Srivastava <97863012+shubhamsrivastava951@users.noreply.github.com>
2022-03-04 17:00:44 +1100

    Update recommend.js

--- static/recommend.js
+++ static/recommend.js
@@ -20,7 +20,7 @@ $(function() {
   })
 
   $('.movie-button').on('click',function(){
-    var my_api_key = 'YOUR_API_KEY';
+    var my_api_key = 'fbd320ff7821ed7b0d7cad171fb98e03';
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -40,7 +40,7 @@ $(function() {
 // will be invoked when clicking on the recommended movie cards
 function recommendcard(e){
   $("#loader").fadeIn();
-  var my_api_key = 'YOUR_API_KEY';
+  var my_api_key = 'Yfbd320ff7821ed7b0d7cad171fb98e03';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
